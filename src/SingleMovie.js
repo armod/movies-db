179143e838cb0be370eda70b6aaa7ac0d0439bd9
armod <armods@gmail.com>
2022-10-25 09:45:44 +0200

    movies-db

--- src/SingleMovie.js
+++ src/SingleMovie.js
@@ -1,30 +1,32 @@
 import React, { useState, useEffect } from 'react'
 import { useParams, Link } from 'react-router-dom'
 import { API_ENDPOINT } from './context'
+import useFetch from './useFetch'
 
 const SingleMovie = () => {
   const { id } = useParams()
-  const [movie, setMovie] = useState({})
-  const { isLoading, setLoading } = useState(true)
-  const [error, setError] = useState({ show: false, msg: '' })
-  // console.log(id)
+  const { isLoading, error, data: movie } = useFetch(`&i=${id}`)
+  // const [movie, setMovie] = useState({})
+  // const { isLoading, setLoading } = useState(true)
+  // const [error, setError] = useState({ show: false, msg: '' })
+  // // console.log(id)
 
-  const fetchMovie = async (url) => {
-    const response = await fetch(url)
-    const data = await response.json()
-    console.log(data)
-    if (data.Response === 'False') {
-      setError({ show: true, msg: data.Error })
-      setLoading(false)
-    } else {
-      setMovie(data)
-      setLoading(false)
-    }
-  }
+  // const fetchMovie = async (url) => {
+  //   const response = await fetch(url)
+  //   const data = await response.json()
+  //   // console.log(data)
+  //   if (data.Response === 'False') {
+  //     setError({ show: true, msg: data.Error })
+  //     setLoading(false)
+  //   } else {
+  //     setMovie(data)
+  //     // setLoading(false)
+  //   }
+  // }
 
-  useEffect(() => {
-    fetchMovie(`${API_ENDPOINT}&i=${id}`)
-  }, [id])
+  // useEffect(() => {
+  //   fetchMovie(`${API_ENDPOINT}&i=${id}`)
+  // }, [id])
 
   if (isLoading) {
     return <div className='loading'></div>
